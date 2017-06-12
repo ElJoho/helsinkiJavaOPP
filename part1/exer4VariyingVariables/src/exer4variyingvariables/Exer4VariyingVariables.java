8e0fc9986d60c227a101bce78be03273b0059c36
ElJoho <johalopezari@unal.edu.co>
2017-06-11 19:36:39 -0500

    Se agregó el ejercicio 4 de la semana 1

---
+++ part1/exer4VariyingVariables/src/exer4variyingvariables/Exer4VariyingVariables.java
@@ -0,0 +1,27 @@
+package exer4variyingvariables;
+
+/**
+ *
+ * @author ElJoho
+ */
+public class Exer4VariyingVariables {
+
+    public static void main(String[] args) {
+       int Chickens = 9000;
+       double Bacon = 5.5;
+       String tracktor = "Zetor";
+       
+       System.out.print("Chickens: \n" + 
+                         Chickens  + "\n" +
+                         "Bacon (kg) : \n" +
+                         Bacon + "\n" +
+                         "A tractor: \n" +
+                         tracktor + "\n \n" +
+                         "In a nutshell: \n" +
+                         Chickens + "\n" + 
+                         Bacon + "\n" + 
+                         tracktor
+                        );
+    }
+    
+}
